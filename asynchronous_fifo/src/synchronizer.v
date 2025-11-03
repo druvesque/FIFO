24d995321704391f70b5f4524ef46dbf4dd3f839
druvesque <dhruvsachdeva6480@gmail.com>
2025-11-03 19:18:02 +0530

    correction

--- asynchronous_fifo/src/synchronizer.v
+++ asynchronous_fifo/src/synchronizer.v
@@ -1,8 +1,8 @@
 `include "asynchronous_fifo_params.vh"
 module synchronizer(
     input clk,
-    input [`ADDR_WIDTH : 0] in,
-    output [`ADDR_WIDTH : 0] reg out
+    input [`PTR_WIDTH : 0] in,
+    output [`PTR_WIDTH : 0] reg out
 );
 
     reg [`ADDR_WIDTH : 0] q1;
